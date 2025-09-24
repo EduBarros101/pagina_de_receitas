63b16201c2b18ccdb3a7de271f9606cc382b8d7a
Eduardo Barros <eduardo.barros@mosten.com>
2025-09-24 17:07:29 -0300

    in progress: stars selection

--- src/js/modules/stars.js
+++ src/js/modules/stars.js
@@ -3,15 +3,27 @@ const commentStarsContainer = document.querySelector(
 );
 
 function createStars() {
-  let stars = 5;
+  let starsQty = 5;
 
-  for (let i = 0; i < stars; i++) {
+  for (let i = 0; i < starsQty; i++) {
     let templateBlackStar = `
-        <i class="fa-notdog fa-solid fa-star" data-index="${i}"></i>
+    <i class="fa-notdog fa-solid fa-star" data-index="${i}"></i>
     `;
 
     commentStarsContainer.innerHTML += templateBlackStar;
   }
+
+  const starsArray = Array.from(commentStarsContainer.querySelectorAll('i'));
+
+  console.log(starsArray);
+
+  starsArray.forEach((element, i) => {
+    element.addEventListener('click', () => {
+      console.log(`clicou na estrela ${i}`);
+
+      starsArray[i].style.color = '#ffd43b';
+    });
+  });
 }
 
 export default createStars;
