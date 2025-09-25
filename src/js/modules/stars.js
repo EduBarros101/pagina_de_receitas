58c51e5dfdd137c87926199d12ab809a829705f1
Eduardo Barros <eduardo.barros@mosten.com>
2025-09-25 11:54:12 -0300

    add: stars vonting working

--- src/js/modules/stars.js
+++ src/js/modules/stars.js
@@ -1,27 +1,33 @@
-const commentStarsContainer = document.querySelector(
-  '#comment-stars-container'
-);
-
 function createStars() {
   let starsQty = 5;
+  let htmlString = '';
+  const commentStarsContainer = document.querySelector(
+    '#comment-stars-container'
+  );
 
   for (let i = 0; i < starsQty; i++) {
-    let templateBlackStar = `
+    htmlString += `
     <i class="fa-notdog fa-solid fa-star" data-index="${i}"></i>
     `;
-
-    commentStarsContainer.innerHTML += templateBlackStar;
   }
 
-  const starsArray = Array.from(commentStarsContainer.querySelectorAll('i'));
+  commentStarsContainer.innerHTML = htmlString;
+
+  const starsArray = commentStarsContainer.querySelectorAll('i');
 
   console.log(starsArray);
 
-  starsArray.forEach((element, i) => {
-    element.addEventListener('click', () => {
-      console.log(`clicou na estrela ${i}`);
+  starsArray.forEach((clickedStar) => {
+    clickedStar.addEventListener('click', () => {
+      const clickedIndex = parseInt(clickedStar.dataset.index);
 
-      starsArray[i].style.color = '#ffd43b';
+      starsArray.forEach((star, currentIndex) => {
+        if (currentIndex <= clickedIndex) {
+          star.style.color = '#ffd43b';
+        } else {
+          star.style.color = '#000000';
+        }
+      });
     });
   });
 }
