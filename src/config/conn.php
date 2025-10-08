838554e779c3fdb2d7903fffb105410e712b59a3
Eduardo Barros <eduardo.barros@mosten.com>
2025-10-08 15:11:52 -0300

    composer and env working

--- src/config/conn.php
+++ src/config/conn.php
@@ -1,10 +1,15 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'pagina_de_receitas';
-$charset = 'utf8mb4';
-$user = 'root';
-$pass = '#Barros101';
+require_once __DIR__ . '../../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$charset = $_ENV['DB_CHARSET'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 $dsn = "mysql:host={$host};dbname={$dbname};charset={$charset}";
 
