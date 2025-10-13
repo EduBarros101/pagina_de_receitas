8d1a20d809a4ced28e70bbdf9918d49802c1275c
Eduardo Barros <eduardo.barros@mosten.com>
2025-10-13 10:55:41 -0300

    cleaning comments

--- src/api/api.php
+++ src/api/api.php
@@ -5,7 +5,3 @@ require_once '../config/conn.php';
 $smtp = $pdo->prepare('select * from receitas');
 $smtp->execute();
 $result = $smtp->fetchAll(PDO::FETCH_ASSOC);
-
-echo "<pre>";
-print_r($result);
-echo "</pre>";
