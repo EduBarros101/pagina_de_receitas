2afaa14c90d2394a4e7e471f1dd8248ab49b192d
Eduardo Barros <eduardo.barros@mosten.com>
2025-10-13 11:08:58 -0300

    fix: typo

--- src/api/api.php
+++ src/api/api.php
@@ -2,6 +2,6 @@
 
 require_once '../config/conn.php';
 
-$smtp = $pdo->prepare('SELECT * FROM receitas');
-$smtp->execute();
-$result = $smtp->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $pdo->prepare('SELECT * FROM receitas');
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
