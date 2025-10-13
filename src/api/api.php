3a94e851b21060941f7ed1631773a77e447e44f9
Eduardo Barros <eduardo.barros@mosten.com>
2025-10-13 11:03:07 -0300

    standandisation

--- src/api/api.php
+++ src/api/api.php
@@ -2,6 +2,6 @@
 
 require_once '../config/conn.php';
 
-$smtp = $pdo->prepare('select * from receitas');
+$smtp = $pdo->prepare('SELECT * FROM receitas');
 $smtp->execute();
 $result = $smtp->fetchAll(PDO::FETCH_ASSOC);
